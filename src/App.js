599b90edf739332e70e35fb3a747954c75697279
krishdevopsmoweb <krish.devops.moweb@gmail.com>
2025-08-21 12:59:41 +0530

    push 5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Heeloi krish how are you hii<code>src/App.js</code> and save to reload.
+          Modiya krish how are you hii<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
