7c41e79a6f09274aba61772800692d8a507c20bb
krishdevopsmoweb <krish.devops.moweb@gmail.com>
2025-08-22 12:18:16 +0530

    push 16

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         hello krish how are you what is going on <code>src/App.js</code> and save to reload.
+         email enable  how are you what is going on <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
